63734af1e83622871f2aa47e9b79a631ff19573d
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-22 02:05:25 +0800

    refactor (skills): change to grid display and add new skills

--- src/components/app/home/Sec3Skills.tsx
+++ src/components/app/home/Sec3Skills.tsx
@@ -1,10 +1,16 @@
 import { 
     backendLogoKeys,
     backendLogos,
+    databaseLogoKeys,
+    databaseLogos,
+    dataToolsLogo,
+    dataToolsLogoKeys,
     frontendLogoKeys, 
     frontendLogos, 
     langKeys, 
-    langLogos 
+    langLogos, 
+    toolsLogo, 
+    toolsLogoKeys
 } from "../../svgs/logos/logoMaps"
 
 export default function Sec3Skills() {
@@ -19,41 +25,64 @@ export default function Sec3Skills() {
                 <h1 className="text-4xl font-bold text-(--text-h)">Skills</h1>
             </div>
 
+            <div className="max-w-2xl mx-auto">
+                <div className="w-full grid grid-cols-[200px_1fr] gap-5 items-center 
+                                [&_h2]:font-bold [&_h2]:text-2xl [&_h2]:text-(--text-h)
+                                [&_svg]:w-15 [&_svg]:h-15 [&>div]:flex [&>div]:flex-wrap [&>div]:flex-row [&>div]:gap-5"
+                >
+                        <h2>Languages:</h2>
+                        <div>
+                            {langKeys.map(lang => (
+                                <div key={lang}>
+                                    {langLogos[lang].comp}
+                                </div>
+                            ))}
+                        </div>
 
-            <div className="[&>div]:mb-5 [&_svg]:w-15 [&_svg]:h-15 [&_svg]:object-contain
-                            [&>div,&>div>div]:flex [&>div,&>div>div]:flex-wrap [&>div,&>div>div]:gap-5"
-            >
-                <div className="flex items-center gap-3">
-                    <h2 className="text-2xl font-bold text-(--text-h)">Languages:</h2>
-                    <div>
-                        {langKeys.map(lang => (
-                            <div key={lang}>
-                                {langLogos[lang].comp}
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                        <h2>Frontend:</h2>
+                        <div>
+                            {frontendLogoKeys.map(front => (
+                                <div key={front}>
+                                    {frontendLogos[front].comp}
+                                </div>
+                            ))}
+                        </div>
 
-                <div>
-                    <h2 className="text-2xl font-bold text-(--text-h)">Frontend:</h2>
-                    <div>
-                        {frontendLogoKeys.map(front => (
-                            <div key={front}>
-                                {frontendLogos[front].comp}
-                            </div>
-                        ))}
-                    </div>
-                </div>
+                        <h2>Backend:</h2>
+                        <div>
+                            {backendLogoKeys.map(back => (
+                                <div key={back}>
+                                    {backendLogos[back].comp}
+                                </div>
+                            ))}
+                        </div>
 
-                <div>
-                    <h2 className="text-2xl font-bold text-(--text-h)">Backend:</h2>
-                    <div>
-                        {backendLogoKeys.map(back => (
-                            <div key={back}>
-                                {backendLogos[back].comp}
-                            </div>
-                        ))}
-                    </div>
+                        <h2>Databases:</h2>
+                        <div>
+                            {databaseLogoKeys.map(db => (
+                                <div key={db}>
+                                    {databaseLogos[db].comp}
+                                </div>
+                            ))}
+                        </div>
+
+                        <h2>Tools:</h2>
+                        <div>
+                            {toolsLogoKeys.map(tool => (
+                                <div key={tool}>
+                                    {toolsLogo[tool].comp}
+                                </div>
+                            ))}
+                        </div>
+
+                        <h2>Data Tools:</h2>
+                        <div>
+                            {dataToolsLogoKeys.map(tool => (
+                                <div key={tool}>
+                                    {dataToolsLogo[tool].comp}
+                                </div>
+                            ))}
+                        </div>
                 </div>
             </div>
         </div>
