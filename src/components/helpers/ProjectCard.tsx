4f41835c24d36defc5b1ddfe70582dc6fe322fb8
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-23 11:28:59 +0800

    feat (projects): add repo and live demo url

--- src/components/helpers/ProjectCard.tsx
+++ src/components/helpers/ProjectCard.tsx
@@ -4,31 +4,41 @@ type ProjectCardProp = {
     imgSrc: string
     title: string
     build: React.ReactNode[]
+    repoUrl: string
+    liveDemoUrl?: string
     className?: string
 }
 
-export function mapProjectBuild({ imgSrc, title, build, className }: ProjectCardProp) {
-    return { imgSrc, title, build, className }
+export function mapFeaturedProj({ imgSrc, title, build, repoUrl, liveDemoUrl, className }: ProjectCardProp) {
+    return { imgSrc, title, build, repoUrl, liveDemoUrl, className }
 } 
 
-export default function ProjectCard({ imgSrc, title, build, className }: ProjectCardProp) {
+export default function ProjectCard({ imgSrc, title, build, repoUrl, liveDemoUrl, className }: ProjectCardProp) {
     return (
         <div className={`${className ?? ""} max-w-125`}>
             <img src={imgSrc} width={500} height={400} />
 
-            <div className="*:mb-1 pl-3 pb-1 bg-(--accent-bg) rounded-b-2xl">
+            <div className="*:mb-1.5 pl-3 pb-1 bg-(--accent-bg) rounded-b-2xl">
                 <h3 className="text-xl font-bold text-(--text-h)">{title}</h3>
 
                 <div className="flex gap-5">
-                    <button className="flex gap-1 items-center px-1 hover:bg-(--text) hover:text-(--bg) rounded-2xl">
+                    {liveDemoUrl && <a className="flex gap-1 items-center px-1 hover:bg-(--text) hover:text-(--bg) rounded-2xl"
+                        href={liveDemoUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <svg width={15} height={15}><use href="/icons.svg#globe"/></svg>
                         <span>Live Demo</span>
-                    </button>
+                    </a>}
 
-                    <button className="flex gap-1 items-center px-1 hover:bg-(--text) hover:text-(--bg) rounded-2xl">
+                    <a className="flex gap-1 items-center px-1 hover:bg-(--text) hover:text-(--bg) rounded-2xl"
+                        href={repoUrl}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
                         <GitHubLogo className="w-3 h-3" />
                         <span>Repo</span>
-                    </button>
+                    </a>
                 </div>
 
                 <div className="flex flex-wrap gap-2 items-center [&_svg]:w-5 [&_svg]:h-5">
