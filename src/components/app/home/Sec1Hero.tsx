4871caa60889b2aa1a6e932f98e66d3eac6337dd
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-25 14:54:35 +0800

    refactor (hero): job titles, layout, and stylings

--- src/components/app/home/Sec1Hero.tsx
+++ src/components/app/home/Sec1Hero.tsx
@@ -1,6 +1,6 @@
 export default function Sec1Hero() {
     return (
-        <div className="p-4">
+        <div className="p-4 gap-5 flex flex-col justify-between">
             {/* Section Header */}
             <div className="flex items-center gap-3">
                 <img src="/profile_icon_wht.png" width={50} height={50} alt="profile-icon" />
@@ -8,14 +8,14 @@ export default function Sec1Hero() {
             </div>
             
             {/* Profile Details */}
-            <div className="flex flex-col sm:flex-row sm:gap-5 p-2 my-2 justify-center items-center">
+            <div className="flex flex-col sm:flex-row sm:gap-5 justify-center items-center">
                 <div className="flex justify-center w-30 h-30 border overflow-hidden rounded-full bg-(--text-h)">
                     <img src="/my-pfp.png" alt="my-pfp" className="w-28 h-37" />
                 </div>
                 
-                <div className="flex flex-col items-center mb-5">
-                    <h2 className="text-3xl font-bold text-(--text-h)">Juan Dela Cruz</h2>
-                    <p>Full-stack Developer | Software Developer</p>
+                <div className="flex flex-col items-center">
+                    <h2 className="text-3xl font-bold text-(--text-h) text-center">Juan Dela Cruz</h2>
+                    <p className="text-center">Full-stack Dev. | Web App Dev. | Data Analyst</p>
                 </div>
             </div>
 
