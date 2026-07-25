6e90287f51c9e5aa7bc026c90d086dd9f9e66e86
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-25 23:34:57 +0800

    feat (types): add ProjectMapType

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,5 +1,19 @@
+import type { ComponentType } from "react"
+
 export interface SVGComponentProps {
     className?: string
     title?: string | boolean
     theme?: "dark" | "light"
+}
+
+export type ProjectMapType = {
+    id: string
+    name: string
+    route: string
+    component: ComponentType<Pick<SVGComponentProps, "className">> | null
+    repoUrl: string
+    liveDemoUrl: string | null
+    build: string[]
+    imgSrc: string
+    class: "featured" | "others"
 }
