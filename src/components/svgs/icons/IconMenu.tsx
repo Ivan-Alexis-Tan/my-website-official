37684c400c4a89fec5384989cc225590662b9bc3
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-07-28 13:16:09 +0800

    refactor (svg): make a props optional

--- src/components/svgs/icons/IconMenu.tsx
+++ src/components/svgs/icons/IconMenu.tsx
@@ -1,4 +1,4 @@
-export default function IconMenu({ className }: { className: string }) {
+export default function IconMenu({ className }: { className?: string }) {
     return (
         <svg className={className ?? ""} xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960" fill="currentColor">
             <path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/>
