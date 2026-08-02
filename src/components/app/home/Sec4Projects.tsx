adf661d5801ca081d8fd0fd99844655afb96da09
Ivan-Alexis-Tan <145218789+Ivan-Alexis-Tan@users.noreply.github.com>
2026-08-02 13:32:44 +0800

    refactor (projects): relocate the layout of `view all` link

--- src/components/app/home/Sec4Projects.tsx
+++ src/components/app/home/Sec4Projects.tsx
@@ -4,18 +4,28 @@ import { searchProject } from "../../../helpers/projectsDbHelpers";
 
 import ProjectCard from "../../helpers/ProjectCard";
 
-
 export default function Sec4Projects() {
     const featuredProjects = searchProject("class", "featured")
 
     return (
         <div className="p-5">
             {/* Section Title */}
-            <div className="flex gap-3 mb-5">
-                <svg width={40} height={40}>
-                    <use href="/icons.svg#projects" />
-                </svg>
-                <h1 className="text-4xl font-bold text-(--text-h)">Projects</h1>
+            <div className="mb-5 flex justify-between items-center">
+                <div className="flex items-center gap-3">
+                    <svg width={40} height={40}>
+                        <use href="/icons.svg#projects" />
+                    </svg>
+                    <h1 className="text-4xl font-bold text-(--text-h)">Projects</h1>
+                </div>
+            
+                <Link to={"/projects/personal-finance-manager"}
+                    className="gap-1 flex items-center hover:text-(--accent)"
+                >
+                    <svg className="w-5 h-5">
+                        <use href="/icons.svg#read-more" />
+                    </svg>
+                    <span>View all</span>
+                </Link>
             </div>
 
             <h2 className="text-2xl font-bold text-(--text-h) mb-2">Featured:</h2>
@@ -31,15 +41,6 @@ export default function Sec4Projects() {
                     />
                 ))}
             </div>
-
-            <Link to={"/projects/personal-finance-manager"}
-                className="gap-1 flex items-center hover:text-(--accent)"
-            >
-                <svg className="w-5 h-5">
-                    <use href="/icons.svg#read-more" />
-                </svg>
-                <span>View all</span>
-            </Link>
         </div>
     )
 }
